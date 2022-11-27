96406425df4bb73f8ecfe6a75c5588f9f7e351bf
SecretKeeper <secretk33per@icloud.com>
2022-11-27 16:30:45 +0330

    init user.repository.ts

---
+++ src/users/users.repository.ts
@@ -0,0 +1,40 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { mapping } from 'cassandra-driver';
+import { User } from './users.service';
+import { CassandraService } from '../cassandra/cassandra.service';
+
+@Injectable()
+export class UsersRepository implements OnModuleInit {
+  constructor(private cassandraService: CassandraService) {}
+
+  usersMapper: mapping.ModelMapper<User>;
+
+  onModuleInit() {
+    const mappingOptions: mapping.MappingOptions = {
+      models: {
+        Employee: {
+          tables: ['employee'],
+          mappings: new mapping.UnderscoreCqlToCamelCaseMappings(),
+        },
+      },
+    };
+
+    this.usersMapper = this.cassandraService
+      .createMapper(mappingOptions)
+      .forModel('user');
+  }
+
+  async getUsers() {
+    return (await this.usersMapper.findAll()).toArray();
+  }
+
+  async register(user: User) {
+    return await this.usersMapper.insert(user);
+  }
+
+  async login(user: User) {
+    return await this.usersMapper.find({
+      id: 'b77d2725-1bfd-4859-870c-dbaa0f8ef0f7',
+    });
+  }
+}
