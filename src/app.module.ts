781948cf5c7d369125136447e81559ba177c2198
SecretKeeper <secretk33p3r@gmail.com>
2022-12-01 12:21:21 +0330

    added prisma service to providers and fix typo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersService } from './users/users.service';
+import { UserService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { CassandraService } from './cassandra/cassandra.service';
 import { CassandraModule } from './cassandra/cassandra.module';
 import { ProfileModule } from './profile/profile.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [AuthModule, UsersModule, CassandraModule, ProfileModule],
   controllers: [AppController],
-  providers: [AppService, UsersService, CassandraService],
+  providers: [AppService, UserService, CassandraService, PrismaService],
 })
 export class AppModule {}
