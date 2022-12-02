accf303789f7c1f3a20c8fb40d65066c694ec417
SecretKeeper <secretk33p3r@gmail.com>
2022-12-02 12:32:20 +0330

    renamed UsersModule to UserModule

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './users.service';
 import { UsersController } from './users.controller';
-import { UserRepository } from './users.repository';
 import { CassandraModule } from '../cassandra/cassandra.module';
 import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   imports: [CassandraModule, PrismaModule],
-  providers: [UserService, UserRepository],
-  exports: [UserService, UserRepository],
+  providers: [UserService],
+  exports: [UserService],
   controllers: [UsersController],
 })
-export class UsersModule {}
+export class UserModule {}
