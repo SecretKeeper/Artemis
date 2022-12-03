4f2b64fe1ec38f2aa71a2c90289197f97635cd55
SecretKeeper <secretk33p3r@gmail.com>
2022-12-03 12:24:32 +0330

    created profile module correctly

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,39 +1,10 @@
-import { Injectable } from '@nestjs/common';
-import { Profile, Prisma } from '@prisma/client';
+import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { ProfileService } from './profile.service';
 
-@Injectable()
-export class ProfileService {
-  constructor(private prisma: PrismaService) {}
-
-  async getProfile(
-    profileWhereUniqueInput: Prisma.ProfileWhereUniqueInput,
-  ): Promise<Profile | null> {
-    return this.prisma.profile.findUnique({
-      where: profileWhereUniqueInput,
-    });
-  }
-
-  async createProfile(data: Prisma.ProfileCreateInput): Promise<Profile> {
-    return this.prisma.profile.create({
-      data,
-    });
-  }
-
-  async updateProfile(params: {
-    where: Prisma.ProfileWhereUniqueInput;
-    data: Prisma.ProfileUpdateInput;
-  }): Promise<Profile> {
-    const { data, where } = params;
-    return this.prisma.profile.update({
-      data,
-      where,
-    });
-  }
-
-  async deleteProfile(where: Prisma.ProfileWhereUniqueInput): Promise<Profile> {
-    return this.prisma.profile.delete({
-      where,
-    });
-  }
-}
+@Module({
+  providers: [ProfileService, PrismaService],
+  exports: [ProfileService],
+  controllers: [],
+})
+export class ProfileModule {}
