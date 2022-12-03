1f651bf7f4228ea779790097b7635d58ac753732
SecretKeeper <secretk33p3r@gmail.com>
2022-12-03 12:24:48 +0330

    created profile service correctly

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { Profile, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(private prisma: PrismaService) {}
+
+  async getProfile(
+    profileWhereUniqueInput: Prisma.ProfileWhereUniqueInput,
+  ): Promise<Profile | null> {
+    return this.prisma.profile.findUnique({
+      where: profileWhereUniqueInput,
+    });
+  }
+
+  async createProfile(data: Prisma.ProfileCreateInput): Promise<Profile> {
+    return this.prisma.profile.create({
+      data,
+    });
+  }
+
+  async updateProfile(params: {
+    where: Prisma.ProfileWhereUniqueInput;
+    data: Prisma.ProfileUpdateInput;
+  }): Promise<Profile> {
+    const { data, where } = params;
+    return this.prisma.profile.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteProfile(where: Prisma.ProfileWhereUniqueInput): Promise<Profile> {
+    return this.prisma.profile.delete({
+      where,
+    });
+  }
+}
