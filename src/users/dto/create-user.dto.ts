33665a7914d577f72fb5eb56ae0426f06156e7fa
SecretKeeper <secretk33p3r@gmail.com>
2022-12-04 12:56:19 +0330

    set default swagger value for username and password

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, Length } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ default: 'SecretKeeper', required: true })
   @IsNotEmpty()
   @Length(3, 33)
   username: string;
@@ -8,6 +10,7 @@ export class CreateUserDto {
   @IsEmail()
   email: string;
 
+  @ApiProperty({ default: 'secret', required: true })
   @IsNotEmpty()
   @Length(8)
   password: string;
