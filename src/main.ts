162863cf2e414fd15edd412cb62c5092be850641
SecretKeeper <secretk33p3r@gmail.com>
2022-12-08 17:12:55 +0330

    implement nest-prisma

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
+
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  const prismaService: PrismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -18,6 +24,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
   await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
