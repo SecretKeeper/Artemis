e9714fa784b9ca2bc4f137b876f96a1b5030bd05
SecretKeeper <secretk33p3r@gmail.com>
2022-12-09 18:33:59 +0330

    use nest-prisma instead of manual implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserService } from './users/users.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/user.module';
 import { CassandraService } from './cassandra/cassandra.service';
 import { CassandraModule } from './cassandra/cassandra.module';
 import { ProfileModule } from './profile/profile.module';
-import { PrismaService } from './prisma.service';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [AuthModule, UsersModule, CassandraModule, ProfileModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    CassandraModule,
+    ProfileModule,
+    PrismaModule.forRoot(),
+  ],
   controllers: [AppController],
-  providers: [AppService, UserService, CassandraService, PrismaService],
+  providers: [AppService, UserService, CassandraService],
 })
 export class AppModule {}
