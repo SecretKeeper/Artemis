00be39ff34f2bc16ad1b84e8ddcffec3c9c5d8be
SecretKeeper <secretk33p3r@gmail.com>
2022-12-10 18:34:21 +0330

    use nest-prisma instead of manual implementation

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Profile, Prisma } from '@prisma/client';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class ProfileService {
