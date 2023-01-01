32b409b287c76e53bf23528937cfa1294cc8e89e
SecretKeeper <secretk33p3r@gmail.com>
2023-01-01 10:28:49 +0330

    implement register and login with username and email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,30 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { PrismaService } from 'nestjs-prisma';
+import { User } from '@prisma/client';
+import { isEmail } from '@/utilities/functions';
 
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService,
     private jwtService: JwtService,
+    private prisma: PrismaService,
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    // const user = await this.usersService.findOne(username);
-    // if (user && user.password === pass) {
-    //   const { password, ...result } = user;
-    //   return result;
-    // }
-    // return null;
+  async authenticateUser(
+    username_or_email: string,
+    password: string,
+    isTryingWithEmail = false,
+  ): Promise<User> {
+    return this.prisma.user.findFirst({
+      where: {
+        AND: [
+          { [isTryingWithEmail ? 'email' : 'username']: username_or_email },
+          { password },
+        ],
+      },
+    });
   }
 
-  async login(user: any) {
-    const payload = {
-      username_or_email: user.username_or_email,
-      sub: user.userId,
-    };
+  async login(credential: LoginDto) {
+    const user = await this.authenticateUser(
+      credential.username_or_email,
+      credential.password,
+      isEmail(credential.username_or_email),
+    );
+
+    if (!user) throw new UnauthorizedException();
+
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(user),
     };
+  }
+
+  async register(user: CreateUserDto) {
+    return await this.userService.createUser(user);
   }
 }
