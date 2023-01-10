b4d1532d270ddc3f16fd164b0dcf619010a24a28
SecretKeeper <secretk33p3r@gmail.com>
2023-01-10 15:47:51 +0330

    return text for index page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,11 @@ import { JwtAuthGuard } from './auth/jwt-auth.gaurd';
 
 @Controller()
 export class AppController {
+  @Get('/')
+  index() {
+    return 'SecretKeeper greets you!';
+  }
+
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
